39825ae0860c3b0328f049ccac368fead100f424
l <louis.bodal@expaceo.com>
2023-01-11 12:30:11 +0100

    4thWithFrequentErrorsNewFile

---
+++ (IT development)/Expaceo/ebp/tickets/10.01.2023_lbo_US267591_ExempleDeMenuResponsive.cs
@@ -0,0 +1,44 @@
+10.01.2023
+
+
+rendre responsive
+
+
+hubbix batiment angular 
+
+responsive. 
+
+spec, 
+
+
+comment afiche 
+
+moobile, 
+
+
+pas maquete
+
+menu horisonto. 
+
+si plus d'info. 
+
+
+
+
+plan d'acction: comment est fait le front. le faire compiler. 
+
+. 
+
+faire une branch 
+
+
+lbo_US267591_ExempleDeMenuResponsive
+
+git branch lbo_US267591_ExempleDeMenuResponsive  origin/develop
+
+11.01.2023
+
+dwa ekrany 
+otworzyc i zobaczyc menu obok ze strona : https://getbootstrap.com/docs/4.3/components/navbar/
+
+1. znalezc kod odpowiedzialny za menu 
---
+++ aws/AWS Certified Cloud Practitioner (CLF-C01)/clf-c01_p13-FrequentErrors.md
@@ -0,0 +1,11 @@
+##   p12_v1
+
+#### Q485::
+`Which options does AWS make available for customers who want to learn about security in the cloud in an instructor-led setting? (Choose two.)
+
+- [ ] AWS Trusted Advisor
+- [x] AWS Online Tech Talks
+- [ ] AWS Blog
+- [ ] AWS Forums
+- [x] AWS Classroom Training
+

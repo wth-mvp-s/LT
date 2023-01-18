7f0d45decdbf57ed78d1af4911ce7332ecebf7a3
l <louis.bodal@expaceo.com>
2023-01-18 20:24:47 +0100

    speedTestLaptop

---
+++ (IT development)/personal/woriorOfTheLand/problemes.cs
@@ -0,0 +1,12 @@
+1. czas leci. 5 lat w apartamencie, nic sie nie zmienilo dla mnie. od 2018 tutaj jestem. 
+
+	- wiecznie sam, nikogo nie mam kogo lubie. 
+
+	tego chyba mi brakuje, miec wrazenie ze jest jakis cel do jakiego daze z kims. 
+
+
+	- nauczyc sie pisac bez patrzenia.
+
+	Pozatym co ? zycie jest fajne w trakcie zakacji. lubie Andaluzje, Sardegne, Lubie Anglie, Singapur. 
+
+wiele miejsc jeszcze chcetnie bym poznal. 	

b095361d3b81fc2632a8b84a69b14a5b8ad6b9ab
lbo.PC <jesuisdev.net@gmail.com>
2023-07-30 14:32:31 +0200

    3007.1432

---
+++ (development)/E/E.methods/e.method.heat.md
@@ -0,0 +1,24 @@
+
+you dont know me
+    I know enough
+        you do what you like?
+        what is your area ? 
+        why ? 
+            have studying ? 
+            where? 
+            how long was there? 
+            you liked it ? 
+
+            where live? 
+
+            familly origin
+            tight familly
+
+why you want to know me
+    i ve seen in a stote from time to time
+
+where I am from
+    my folks
+
+    my dream
+    
--- (development)/E/E.methods/e.method.jackal.cs
+++ (development)/E/E.methods/e.method.jackal.cs
@@ -1,84 +1,36 @@
-﻿using System.Reflection;
-
-namespace ConsoleApp1tealswanmethods
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-    interface IWhy
-    {
+﻿
+Why
         //doesn't matter
         //I am the best for the job
         //I like what I do, it amuse me
         //it's me, my life
-    }
 
-    interface IExperience // Fetch
-    {
-        int znapassport { get;  }
-        int znagdziezrobicpassport { get;  }
-        int znabron { get; }
-        int znagdziekupicbron { get;  } 
-        int wiejakprzygotowacbron { get;  }
-        int znagdzieulokowacpieniadze { get;  }
-        
-    }
-    interface IAccess
-    {
-        int jakkupicAuto { get; }
-        int jakkupicbron { get; }
-    }
-    
-    public struct Transport
-    {
-        public int ochronaprzedzlodziejembroni => throw new NotImplementedException();
+Experience 
 
-        int Loty { get; }
-        int Samochodnabron { get; }
-        int Samochodnaatak { get; }
-        int Yacht { get; }
-        int KontaktWhiteHouse { get; }
-        int KontaktPassport { get; }
-    }
-    interface Itransport 
-    {
-        Transport Transport { get; }
-    }
-    interface IBron
-    {
-        int KontaktBron { get; }
-    }
-    interface Iwycena : Itransport, I
-    { 
-        string Paszportjakacena { get; }
-        string Bronjakacena { get; }
-        string przygotowaniepodbron { get; }
-        Transport Transoportjakacena { get; }
-    }
-    interface ICel : Iustalicmaterialy
-    {
-        string okresliczapotrzebowanieabyosiagnaccel();
+    znapassport 
+    znagdziezrobicpassport 
+    znabron
+    znagdziekupicbron 
+    wiejakprzygotowacbron 
+    znagdzieulokowacpieniadze 
         
-    }
-    public struct GeoCoordinate
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-    interface Iou
-    {
-        GeoCoordinate wybracmiejscepodkontemcelu(); 
-    }
-    interface Iprep : Iou
-    {
-        GeoCoordinate wybracmiejscepodkontemcelu();
+Access
+    jakkupicAuto 
+    jakkupicbron 
 
-        
-        
-    }
-    
-}
+
+Transport
+
+    Loty 
+    Samochodnabron 
+    Samochodnaatak 
+    Yacht 
+    KontaktWhiteHouse 
+    KontaktPassport 
+
+wycena
+
+    Paszportjakacena 
+    Bronjakacena 
+    przygotowaniepodbron 
+    Transoportjakacena 

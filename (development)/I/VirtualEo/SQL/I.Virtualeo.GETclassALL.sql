f076bc93bde112b145d6667976eae5fef7974246
louis.bodal <louis.bodal@expaceo.com>
2023-08-18 17:18:56 +0200

    1808.1718

--- (development)/I/VirtualEo/SQL/I.Virtualeo.GETclassALL.sql
+++ (development)/I/VirtualEo/SQL/I.Virtualeo.GETclassALL.sql
@@ -5,14 +5,15 @@ SELECT
 	ct.Code AS TypeDeClasse_FromCode, 
 	ct.Label AS TypeDeClasse_FromLabel, 
 	vi.Publisher AS SystemeDexploitation,
+	vi.Id AS VMimageId,
+	vi.Label AS VMimageLabel,
 	vs.Description AS PuissanceDesMachines,
-	'from :',
 	c.Id, 
 	c.Name, 
 	d.Id
 
 FROM Classroom AS c
-INNER JOIN Department AS d
+INNER JOIN Department AS d	
 	ON c.DepartmentId = d.Id
 
 INNER JOIN ClassroomType AS ct
@@ -26,4 +27,14 @@ INNER JOIN VMSize AS vs
 	ON c.VmSizeId = vs.Id
 
 --WHERE c.Name like '%az-204%'
-WHERE c.Id = 22
+WHERE c.Id = 11
+
+-- 
+select * from VMImage where Id=6
+
+/*
+*temp delete Label From Id 6 where 2022 DATACENTER
+*/
+Update VMImage 
+set Label = '2022 DATACENTER'
+where Id = 6; 
